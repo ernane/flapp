0d928a5be4769dc053b6694c8588e645757a7cb8
Ernane Sena <ernane.sena@gmail.com>
2020-07-21 21:09:20 -0300

    :recycle: Refactor module Toolbar

--- flapp/ext/toolbar/__init__.py
+++ flapp/ext/toolbar/__init__.py
@@ -1,6 +1,5 @@
-from flask_debugtoolbar import DebugToolbarExtension
-
-
 def init_app(app):
     if app.debug:
+        from flask_debugtoolbar import DebugToolbarExtension
+
         DebugToolbarExtension(app)
